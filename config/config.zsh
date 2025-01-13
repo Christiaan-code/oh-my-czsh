e565dc69383ceebd107ad013bfebc464dc9a0ed4
Christiaan du Preez <christiaan.d@jobjack.co.za>
2025-01-13 13:45:22 +0200

    Fix exports sourcing

--- config/config.zsh
+++ config/config.zsh
@@ -3,8 +3,8 @@ for theme_file in ~/.zsh/config/themes/**/*.zsh; do
   source $theme_file
 done
 
+source ~/.zsh/config/exports/exports.zsh
+
 source $ZSH/oh-my-zsh.sh
 source $(brew --prefix nvm)/nvm.sh
 PATH=~/.console-ninja/.bin:$PATH
-
-source ~/.zsh/config/exports/exports.zsh
