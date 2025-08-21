765e45e3aac97824a17dcbc894b8feaf6ba2ce0d
Christiaan <christiaan.d@jobjack.co.za>
2025-08-21 17:04:39 +0200

    Update config file to match default file locations from readme file

--- config/config.zsh
+++ config/config.zsh
@@ -1,10 +1,10 @@
-source ~/jobjack/oh-my-czsh/config/preferences/configure-zsh.zsh
-source ~/jobjack/oh-my-czsh/config/plugins/plugins.zsh
-for theme_file in ~/jobjack/oh-my-czsh/config/themes/**/*.zsh; do
+source ~/.zsh/config/preferences/configure-zsh.zsh
+source ~/.zsh/config/plugins/plugins.zsh
+for theme_file in ~/.zsh/config/themes/**/*.zsh; do
   source $theme_file
 done
 
-source ~/jobjack/oh-my-czsh/config/exports/exports.zsh
+source ~/.zsh/config/exports/exports.zsh
 
 source $ZSH/oh-my-zsh.sh
 source $(brew --prefix nvm)/nvm.sh
